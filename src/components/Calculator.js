968649119f4a20da271555ea8dc6a96964c9d152
BryteLitty <littyofficials@gmail.com>
2022-12-14 12:54:33 +0000

    add UI using components

---
+++ src/components/Calculator.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './Calculator.css';
+
+// function to generate digits
+
+const generateDigits = () => {
+  const digits = [];
+
+  for (let i = 1; i < 10; i + 1) {
+    digits.push(
+      <button className="button" type="button" key={i}>{i}</button>,
+    );
+  }
+
+  return digits;
+};
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="display-result">
+          <span className="result">0</span>
+        </div>
+
+        <div className="buttons">
+          <div className="controllers">
+            <button className="button" type="button">AC</button>
+            <button className="button" type="button">+/-</button>
+            <button className="button" type="button">%</button>
+            {generateDigits()}
+            <button className="extend" type="button">0</button>
+            <button className="dot" type="button">.</button>
+          </div>
+
+          <div className="operators">
+            <button className="operator" type="button">+</button>
+            <button className="operator" type="button">x</button>
+            <button className="operator" type="button">-</button>
+            <button className="operator" type="button">/</button>
+            <button className="operator" type="button">=</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
