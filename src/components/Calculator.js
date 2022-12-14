32d50fe6e374cf706cbf305315dc1e1df6d36e27
BryteLitty <littyofficials@gmail.com>
2022-12-14 14:11:04 +0000

    fix error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,7 +6,7 @@ import './Calculator.css';
 const generateDigits = () => {
   const digits = [];
 
-  for (let i = 1; i < 10; i + 1) {
+  for (let i = 1; i < 10; i += 1) {
     digits.push(
       <button className="button" type="button" key={i}>{i}</button>,
     );
